9951c8a2b9c73a6897978aa25e9c830db047f77c
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-11-28 16:58:28 +0530

    :zap: :package: bcrypt :art: added signup functionality and connected to database

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,32 @@
+import { hashPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db"
+
+async function handler(req, res) {
+
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (!email || !email.includes('@') || !password || password.trim().length < 5) {
+    res.status(422).json({
+      message: 'Invalid credentials'
+    })
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = await hashPassword(password)
+
+  const result = await db.collection('users').insertOne({
+    email,
+    password: hashedPassword
+  })
+
+  res.status(201).json({
+    message: "Created user!!"
+  })
+}
+
+export default handler
