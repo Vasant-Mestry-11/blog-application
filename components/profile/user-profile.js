5007323ff29b6670c309c39634f60cf5d3fb7197
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-12-04 19:07:30 +0530

    :zap: :art: user profile redirect if not authenticated and loader while loading

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,21 @@
+import { useSession } from 'next-auth/react';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+import { useEffect } from 'react';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const { session, status } = useSession();
+
+  useEffect(() => {
+    if (!session) {
+      window.location.href = '/auth'
+    }
+
+  }, [session])
+
+  if (status === 'loading') {
+    return <h4 className={classes.profile}>Loading...</h4>
+  }
 
   return (
     <section className={classes.profile}>
