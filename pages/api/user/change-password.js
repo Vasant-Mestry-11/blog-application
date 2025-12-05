29167bf58cb22f9debeea1519a2c40f69fdd3c9b
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-12-05 09:26:06 +0530

    :zap: :art: change password route and its logic with DB updation

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 // route:  /api/user/change-password
+import { hashPassword, verifyPassword } from '@/lib/auth';
+import { connectToDatabase } from '@/lib/db';
 import authOptions from '@/pages/api/auth/[...nextauth]'
 import { getServerSession } from "next-auth";
 
@@ -19,6 +21,53 @@ async function handler(req, res) {
     })
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const userCollection = client.db().collection('users');
+
+  const user = await userCollection.findOne({
+    email: userEmail
+  });
+
+  if (!user) {
+    res.status(404).json({
+      message: "User not found."
+    })
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({
+      message: 'Invalid Password.'
+    })
+    client.close()
+    return
+  }
+
+  const hashedPassword = await hashPassword(newPassword)
+
+  const result = await userCollection.updateOne({
+    email: userEmail
+  }, {
+    $set: {
+      password: hashedPassword
+    }
+  })
+
+
+  client.close();
+  res.status(200).json({
+    message: "Password updated!"
+  })
 
 }
 
